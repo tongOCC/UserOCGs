711c366688845585904f0d4d7f2ec5f08ff54007
Daniel Reyna <danielreyna214@gmail.com>
2017-11-30 16:39:28 -0800

    fixes list linear layout for profile, and clears activities on logout, it lives

--- app/src/main/java/dreyna/tong/userocgs/MainMenuActivity.java
+++ app/src/main/java/dreyna/tong/userocgs/MainMenuActivity.java
@@ -18,12 +18,12 @@ public class MainMenuActivity extends AppCompatActivity {
     private List<Profile> allProfiles;
     private ListView logListView;
 
-    private List<Logger> allLogsList= new ArrayList<>();
+    private List<Logger> allLogsList = new ArrayList<>();
     private List<Logger> filteredLogs = new ArrayList<>();
 
     private Uri imageUri;
     private LogListAdapter LogListAdapter;
-private String name;
+    private String name;
     private Profile yourProfile;
 
     @Override
@@ -35,53 +35,56 @@ private String name;
         db = new DBHelper(this);
 
         allProfiles = db.getAllProfile();
-        allLogsList= db.getAllLogs();
+        allLogsList = db.getAllLogs();
 
         Intent intent = getIntent();
-        String intentGrabName= intent.getStringExtra("name");
-        name= intentGrabName;
-        yourProfile= intent.getExtras().getParcelable("profileName");
+        String intentGrabName = intent.getStringExtra("name");
+        name = intentGrabName;
+        yourProfile = intent.getExtras().getParcelable("profileName");
 
         for (Logger l : allLogsList) {
             if (yourProfile.getName().equals(l.getName()))
                 filteredLogs.add(l);
         }
 
-        LogListAdapter = new LogListAdapter(this, R.layout.profile_list_item,  filteredLogs);
+        LogListAdapter = new LogListAdapter(this, R.layout.profile_list_item, filteredLogs);
         logListView.setAdapter(LogListAdapter);
         Toast.makeText(MainMenuActivity.this, filteredLogs.toString(), Toast.LENGTH_LONG).show();
     }
 
     public void logOutOnClick(View view) {
         Toast.makeText(MainMenuActivity.this, R.string.successfullyloggedOut, Toast.LENGTH_SHORT).show();
-        name="";
-startActivity(new Intent(this,LoginActivity.class));
+        name = "";
+        Intent logOut = new Intent(this, LoginActivity.class);
+        logOut.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        startActivity(logOut);
 
     }
 
     public void HandleRecycleButton(View view) {
 
-        Intent intent= new Intent(this, NewLogActivity.class);
-       // intent.putExtra("profileObject", yourProfile);
-        intent.putExtra("profileName", yourProfile );
+        Intent intent = new Intent(this, NewLogActivity.class);
+        // intent.putExtra("profileObject", yourProfile);
+        intent.putExtra("profileName", yourProfile);
         startActivity(intent);
 
     }
-    public void viewDetails(View view)
-    {
-        LinearLayout select= (LinearLayout) view;
-        Logger selectedLog= (Logger) select.getTag();
+
+    public void viewDetails(View view) {
+        LinearLayout select = (LinearLayout) view;
+        Logger selectedLog = (Logger) select.getTag();
 
 //        String money=String.valueOf(selectedLog.getMoney_earned());
-        String RecyceledTotal= String.valueOf(selectedLog.getTotal_recycled());
-        String date= selectedLog.getDate().toString();
+        String RecyceledTotal = String.valueOf(selectedLog.getTotal_recycled());
+        String date = selectedLog.getDate().toString();
         String Uri = selectedLog.getReciept_image().toString();
 
-        Intent detailsIntent= new Intent(this, RecycleDetailsActivity.class);
-       // detailsIntent.putExtra("money_earned",money);
-        detailsIntent.putExtra("date",date);
-        detailsIntent.putExtra("recycled_total",RecyceledTotal);
-        detailsIntent.putExtra("Uri",Uri);
+        Intent detailsIntent = new Intent(this, RecycleDetailsActivity.class);
+        // detailsIntent.putExtra("money_earned",money);
+        detailsIntent.putExtra("date", date);
+        detailsIntent.putExtra("recycled_total", RecyceledTotal);
+        detailsIntent.putExtra("Uri", Uri);
         startActivity(detailsIntent);
     }
 
