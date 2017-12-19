6fced172ecdaf0b6c8725c02778301694ae83801
TongOCC <agdtev@gmail.com>
2017-12-18 18:35:28 -0800

    final decision to remove images for listViews done

--- app/src/main/java/dreyna/tong/userocgs/LogListAdapter.java
+++ app/src/main/java/dreyna/tong/userocgs/LogListAdapter.java
@@ -1,14 +1,12 @@
 package dreyna.tong.userocgs;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
-import android.content.res.AssetManager;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,11 +14,10 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RatingBar;
 import android.widget.TextView;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +33,7 @@ public class LogListAdapter extends ArrayAdapter<Logger> {
     private Context mContext;
     private List<Logger> mLogsList = new ArrayList<>();
     private int mResourceId;
+    private Uri imageUri;
     private LinearLayout logListLinearLayout;
     private TextView nameTextView;
     private TextView descriptionTextView;
@@ -67,36 +65,54 @@ private String tev= "hi";
         MainMenuActivity.ViewHolder holder;
         LayoutInflater inflater =
                 (LayoutInflater) mContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-        if (convertView == null) {
-            convertView = inflater.inflate(R.layout.profile_list_item, null);
-            holder = new MainMenuActivity.ViewHolder();
-            holder.nameTextView = (TextView) convertView.findViewById(R.id.profileListViewName);
-            holder.descriptionTextView = (TextView) convertView.findViewById(R.id.profileListDescriptionTextView);
-            holder.personImageView = (ImageView) convertView.findViewById(R.id.ProfileListImageView);
-            convertView.setTag(holder);
-        } else {
-            holder = (MainMenuActivity.ViewHolder) convertView.getTag();
-        }
-        DecimalFormat twoPlaces = new DecimalFormat("0.00");
-
-        View view = inflater.inflate(mResourceId, null);
+    View view= inflater.inflate(R.layout.profile_list_item, null);
         Logger logs = mLogsList.get(pos);
-        holder.nameTextView.setText(logs.getName());
-        holder.descriptionTextView.setText("Date: " + logs.getDate() + "\nMoney Earned: $" + String.valueOf(twoPlaces.format(logs.getMoney_earned()))
-                + "\nTotal Recycled: LB(s) " + String.valueOf(logs.getTotal_recycled()));
-        holder.personImageView.setImageURI(logs.getReciept_image());
-//        logListLinearLayout =
-//                (LinearLayout) view.findViewById(R.id.listItemLinearLayout);
-//        descriptionTextView =
-//                (TextView) view.findViewById(R.id.profileListDescriptionTextView);
-//        nameTextView =
-//                (TextView) view.findViewById(R.id.profileListViewName);
-//        recieptImageView =
-//                (ImageView) view.findViewById(R.id.ProfileListImageView);
+        DecimalFormat twoPlaces = new DecimalFormat("0.00");
+//        if (convertView == null) {
+//            convertView = inflater.inflate(R.layout.profile_list_item, null);
+//            logListLinearLayout =
+//                    (LinearLayout) convertView.findViewById(R.id.listItemLinearLayout);
+//            holder = new MainMenuActivity.ViewHolder();
+//            holder.nameTextView = (TextView) convertView.findViewById(R.id.profileListViewName);
+//            holder.descriptionTextView = (TextView) convertView.findViewById(R.id.profileListDescriptionTextView);
+//            holder.personImageView = (ImageView) convertView.findViewById(R.id.ProfileListImageView);
+//            convertView.setTag(holder);
+//        } else {
+//            holder = (MainMenuActivity.ViewHolder) convertView.getTag();
+//        }
+//        DecimalFormat twoPlaces = new DecimalFormat("0.00");
 //
-//        nameTextView.setText(logs.getName());
-//        descriptionTextView.setText("Date: " + logs.getDate() + "\nMoney Earned: $" + String.valueOf(twoPlaces.format(logs.getMoney_earned()))
+//        Logger logs = mLogsList.get(pos);
+//        imageUri=logs.getReciept_image();
+//        holder.nameTextView.setText(logs.getName());
+//        holder.descriptionTextView.setText("Date: " + logs.getDate() + "\nMoney Earned: $" + String.valueOf(twoPlaces.format(logs.getMoney_earned()))
 //                + "\nTotal Recycled: LB(s) " + String.valueOf(logs.getTotal_recycled()));
+//        if(checksIftheFileisThere(imageUri))
+//        {
+//            imageUri=getUriFromResource(mContext,R.drawable.markedfordeletion);
+//            holder.personImageView.setImageURI(imageUri);
+//            logListLinearLayout.setTag(logs);
+//        }
+//        else
+//        holder.personImageView.setImageURI(logs.getReciept_image());
+//        logListLinearLayout.setTag(logs);
+
+
+
+
+
+        logListLinearLayout =
+                (LinearLayout) view.findViewById(R.id.listItemLinearLayout);
+        descriptionTextView =
+                (TextView) view.findViewById(R.id.profileListDescriptionTextView);
+        nameTextView =
+                (TextView) view.findViewById(R.id.profileListViewName);
+        //recieptImageView =
+           //     (ImageView) view.findViewById(R.id.ProfileListImageView);
+
+        nameTextView.setText(logs.getName());
+        descriptionTextView.setText("Date: " + logs.getDate() + "\nMoney Earned: $" + String.valueOf(twoPlaces.format(logs.getMoney_earned()))
+                + "\nTotal Recycled: LB(s) " + String.valueOf(logs.getTotal_recycled())+"\n\n");
 //        try {
 //            Bitmap bitmap = MediaStore.Images.Media.getBitmap(mContext.getContentResolver(), logs.getReciept_image());
 //            bitmap = Bitmap.createScaledBitmap(bitmap, 100, 150, true);
@@ -105,11 +121,30 @@ private String tev= "hi";
 //            Log.e("MovieListAdapter", "Error getting bitmap from: " + logs.getReciept_image().toString(), e);
 //        }
 //            recieptImageView.setImageURI(logs.getReciept_image());
-//            logListLinearLayout.setTag(logs);
-//            return view;
-//        }
-        return convertView;
+            logListLinearLayout.setTag(logs);
+            return view;
+        }
+       // return convertView;
 
+    public static Uri getUriFromResource(Context context, int resID)
+    {
+        Resources res= context.getResources();
+        //Build a String in the form:
+        //android.resource://edu.orangecoastcollege.cs273.petprotector
+        String uri= ContentResolver.SCHEME_ANDROID_RESOURCE+"://"+ res.getResourcePackageName(resID)+ "/"
+                + res.getResourceTypeName(resID)+"/"
+                +res.getResourceEntryName(resID);
+        // parse the string in o rder to construc a URI
+        return Uri.parse(uri);
+
+    }
+    public static boolean checksIftheFileisThere(Uri path)
+    {
+        String filePath= path.toString();
+        File file= new File(filePath);
+        if(file.exists())
+            return true;
+        return false;
     }
 }
 
